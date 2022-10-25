5b458534db27b3e90512342db2a86b20df421913
Shahul Alam <alam.shahul@gmail.com>
2022-10-25 13:21:15 -0400

    Added more instructions.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,4 @@
 import { useState, useEffect, useRef } from 'react';
-import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 
 function Home(props) {
@@ -12,7 +11,9 @@ function Home(props) {
       </div>
       <div className="home-text">
         <div>Instructions:</div>
-        <ListItem sx={{ display: 'list-item' }}>Hello</ListItem>
+        <ListItem sx={{ display: 'list-item' }}>This is a <span className="bold">C</span>apture <span className="bold">T</span>he <span className="bold">F</span>lag! Your goal is to hack through all the puzzles as fast as you can.</ListItem>
+        <ListItem sx={{ display: 'list-item' }}>If you're stuck on a puzzle, ask for a hint! Don't worry, we'll try to not completely give the solution away.</ListItem>
+        <ListItem sx={{ display: 'list-item' }}>If you complete a puzzle, the corresponding button text on the sidebar will be styled as <span className="completed_puzzle">green strikthrough.</span></ListItem>
         <div className="crossword_container">
         </div>
       </div>
